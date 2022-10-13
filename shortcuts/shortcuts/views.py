0cdf2df85c941446c3c42cdff6d57307ded84b00
Paulo Simunovic <91575657+psimunovic@users.noreply.github.com>
2022-10-13 15:12:07 -0300

    Ejemplo de Shortcuts

---
+++ shortcuts/shortcuts/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Context, Template
+from django.template import loader
+#from django.template.loader import get_template #para cargar multiples templates
+from django.shortcuts import render#simplifica la codificacion
+
+#**************************************************
+class Persona(object):#Constructor
+    def __init__(self, nombre, apellido):
+        self.nombre=nombre
+        self.apellido=apellido
+
+def atajo(request):
+    
+    p1=Persona("Profe Tadeo", "Cepeda")
+    temasCurso=["Plantillas","Modelos","ORM","Sesiones", "Seguridad"]
+    fecha=datetime.datetime.now()
+    #doc_ext=open("C:/django/plantilla/plantillas/plantilla1.html")
+    #plt=Template(doc_ext.read())
+    #doc_ext.close()
+    #Con esto hay que ir al archivo settings.py y dentro de la lista template buscar DIRS,
+    #donde se colocara la ruta hacia la carpeta de las plantillas
+    #contx=Context({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "fecha_act":fecha, "temas":temasCurso})#llamar lista
+    #doc_ext=loader.get_template('plantilla2.html')
+    #pagina=doc_ext.render({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "fecha_act":fecha, "temas":temasCurso})
+    return render(request, "plantilla2.html",{"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "fecha_act":fecha, "temas":temasCurso})
+
+
